3e1bcca81788f06a0ddc3dbd19931a54571f5851
@NISHAN <140871999+ASHADEEP-NISHAN@users.noreply.github.com>
2023-08-19 01:57:29 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,282 @@
+# KOUN BANEGA CROREPATI
+print("welcome to ASHADEEP'S KBC")
+print("KON BANEGA CROREPATI MAIN APKA SWAGAT HAI")
+print("ARE YOU READY !")
+x=int(input("IF YES PRESS 1"))
+if x==1:
+      print("FIRST QUESTION")
+      print("\n")
+      print("Which is our National bird")
+      print("a-crow    b-hen")
+      print("c-parrot  d-peacock")
+      c = ['a', 'b', 'c', 'd']
+      print("which one is correct",c)
+      print("\n")
+
+      b=input()
+      amount=0
+
+      if b=='d':
+                print("correct answer is 4-peacock")
+                print("congratulation!")
+                print("you own $5000")
+                amount=amount+5000
+                print("you won total", amount)
+
+
+      else:
+                print("correct answer is 4-peacock")
+                print("better luck next time")
+                print("you won $0")
+                exit()
+
+
+
+      # Question 1
+      print("\n")
+      print("\n")
+      print("1. What is the capital of France?")
+      print("a) London")
+      print("b) Berlin")
+      print("c) Paris")
+      print("d) Rome")
+      print("which one is correct", c)
+
+      b=input("enter the option from above list")
+      if b=='c':
+          print("Correct Answer: c) Paris")
+          print("congratulation!")
+          print("you own $5000")
+          amount=amount+5000
+          print("you won $", amount)
+
+      else:
+          print("Correct Answer: c) Paris")
+          print("better luck next time")
+          print("you won total ", amount)
+          exit()
+      # Question 2
+      print("\n")
+      print("\n")
+      print("2. Which planet is known as the 'Red Planet'?")
+      print("a) Jupiter")
+      print("b) Mars")
+      print("c) Venus")
+      print("d) Mercury")
+      print("which one is correct", c)
+      print("\n")
+
+      b = input("enter the option from above list")
+      if b == 'b':
+
+         print("Correct Answer: b) Mars")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+
+      else:
+         print("Correct Answer: b) Mars")
+         print("better luck next time")
+         print("you won total ", amount)
+         exit()
+      # Question 3
+      print("\n")
+      print("\n")
+      print("3. Who painted the Mona Lisa?")
+      print("a) Vincent van Gogh")
+      print("b) Pablo Picasso")
+      print("c) Leonardo da Vinci")
+      print("d) Michelangelo")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'c':
+         print("Correct Answer: c) Leonardo da Vinci")
+         print("congratulation!")
+         print("you own $5000")
+         amount=amount+ 5000
+         print("you won total $", amount)
+      else:
+         print("Correct Answer: c) Leonardo da Vinci")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+
+
+
+      # Question 4
+      print("\n")
+      print("\n")
+      print("4. What is the chemical symbol for water?")
+      print("a) O2")
+      print("b) H2O")
+      print("c) CO2")
+      print("d) N2")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'b':
+         print("Correct Answer: b) H2O")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: b) H2O")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+
+
+      # Question 5
+      print("\n")
+      print("\n")
+      print("5. Which country is known as the 'Land of the Rising Sun'?")
+      print("a) China")
+      print("b) Japan")
+      print("c) South Korea")
+      print("d) Thailand")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'b':
+         print("Correct Answer: b) Japan")
+         print("congratulation!")
+         print("you own $5000")
+         amount=amount+ 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: b) Japan")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+
+      # Question 6
+      print("\n")
+      print("\n")
+      print("6. What is the largest mammal in the world?")
+      print("a) Elephant")
+      print("b) Blue Whale")
+      print("c) Lion")
+      print("d) Giraffe")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'b':
+         print("Correct Answer: b) Blue Whale")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: b) Blue Whale")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+
+
+      # Question 7
+      print("\n")
+      print("\n")
+      print("7. Who wrote the play 'Romeo and Juliet'?")
+      print("a) William Shakespeare")
+      print("b) Charles Dickens")
+      print("c) Mark Twain")
+      print("d) Jane Austen")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'a':
+         print("Correct Answer: a) William Shakespeare")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: a) William Shakespeare")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+      print("\n")
+      print("\n")
+      print("8. What is the currency of Japan?")
+      print("a) Yen")
+      print("b) Won")
+      print("c) Rupee")
+      print("d) Euro")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'a':
+         print("Correct Answer: a) Yen")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: a) Yen")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+
+      # Question 9
+      print("\n")
+      print("\n")
+      print("9. What is the largest organ in the human body?")
+      print("a) Heart")
+      print("b) Lungs")
+      print("c) Liver")
+      print("d) Skin")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'd':
+         print("Correct Answer: d) Skin")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: d) Skin")
+         print("better luck next time")
+         print("you won total $", amount)
+         exit()
+
+      # Question 10
+      print("\n")
+      print("\n")
+      print("10. What is the tallest mountain in the world?")
+      print("a) Mount Kilimanjaro")
+      print("b) Mount Everest")
+      print("c) Mount McKinley (Denali)")
+      print("d) Mount Fuji")
+      print("which one is correct", c)
+      print("\n")
+      b = input("enter the option from above list")
+      if b == 'b':
+         print("Correct Answer: b) Mount Everest")
+         print("congratulation!")
+         print("you own $5000")
+         amount = amount + 5000
+         print("you won total $", amount)
+
+      else:
+         print("Correct Answer: b) Mount Everest")
+         print("better luck next time")
+         print("you won total $", amount)
+         print("Thank You !")
+         exit()
+else:
+     print("\n")
+     print("\n")
+     print("Thank You")
+
+
